643a2eaf8842600ea7d0ea9a64f737ad2187454c
pvgmenegasso <pvgmenegasso@gmail.com>
2022-09-06 20:34:33 -0300

    Split module in different files and deleted unused .toml file

--- src/__init__.py
+++ src/__init__.py
@@ -1,62 +1,19 @@
-import logging
-from typing import Optional
-from dataclasses import dataclass
+from colors import (
+    AscIIColors as AscIIColors,
+    ColorFormatter as ColorFormatter,
+    init_logs as init_logs
+    )
 
-@dataclass
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    MAGENTA="\033[35m"
-    BRIGHTRED="\033[31;1m"
-
-class ColorFormatter(logging.Formatter):
-
-    BASEFORMAT : str = f'%(asctime)s %(filename)s | %(levelname)s:'
-    EXTENDEDFORMAT : str = f'%(asctime)s %(filename)s line %(lineno)d | %(levelname)s:'
-    MESSAGE : str = f'%(message)s'
-    FORMATS = {
-            logging.INFO : f'{bcolors.OKBLUE}{BASEFORMAT}{bcolors.ENDC} {MESSAGE}',
-            logging.DEBUG : f'{bcolors.OKGREEN}{BASEFORMAT}{bcolors.ENDC} {MESSAGE}',
-            logging.WARNING : f'{bcolors.WARNING}{EXTENDEDFORMAT}{bcolors.ENDC} {MESSAGE}',
-            logging.ERROR : f'{bcolors.FAIL}{bcolors.BOLD}{EXTENDEDFORMAT}{bcolors.ENDC} {MESSAGE}'
-        }
-    DATEFMT = '%d/%m-%X'
-
-    def __init__(self, fmt: Optional[str] = ..., datefmt: Optional[str] = ..., validate: bool = ...) -> None:
-        super().__init__()
-
-    def format(self, record: Optional[logging.LogRecord]) -> str:
-        if record is not None:
-            newformat = logging.Formatter(fmt = self.FORMATS.get(record.levelno)
-            , datefmt=self.DATEFMT)
-            return newformat.format(record)
-       
-
-def init_logs(severity : int):
-
-    try:
-        assert severity in (
-            logging.CRITICAL,
-            logging.DEBUG,
-            logging.ERROR,
-            logging.FATAL,
-            logging.INFO,
-            logging.WARN,
-            logging.WARNING,
-            )
-    except:
-        Warning('Log severity level not found')
-
-    log = logging.getLogger()
-    log.setLevel(severity)
-    sh = logging.StreamHandler()
-    sh.setFormatter(ColorFormatter())
-    log.removeHandler(log.handlers)
-    log.addHandler(sh)
+from logging import (
+    INFO as INFO, 
+    DEBUG as DEBUG,
+    ERROR as ERROR, 
+    FATAL as FATAL, 
+    CRITICAL as CRITICAL, 
+    WARNING as WARNING, 
+    info as info, 
+    debug as debug, 
+    error as error, 
+    warning as warning, 
+    critical as critical, 
+    fatal as fatal)
